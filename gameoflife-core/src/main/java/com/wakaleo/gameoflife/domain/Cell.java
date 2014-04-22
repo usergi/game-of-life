157d8eb2b028db0787972f26ff031fa4f12b3993
usergi <emymimi.fabulous@gmail.com>
2014-04-22 15:16:51 +0100

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,9 +1,9 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("-"), DEAD_CELL(".");
+    LIVE_CELL("*"), DEAD_CELL(".");
 
-    privat String symbol;
+    private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
